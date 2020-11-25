d810c0a62600a8f7e5eb72cca24bf2e64776f22f
Restyled.io <commits@restyled.io>
2020-11-25 09:25:22 +0000

    Restyled by clang-format

--- apps/store-ssr/webpack.config.js
+++ apps/store-ssr/webpack.config.js
@@ -3,23 +3,23 @@ const nrwlConfig = require("@nrwl/react/plugins/webpack.js")
 module.exports = (config, context) => {
   nrwlConfig(config)
   return {
-    ...config,
-    module: {
+    ...config, module: {
       ...config.module,
-      rules: [
-        ...config.module.rules,
-        {
-          test: /\.s[ac]ss$/i,
-          use: [
-            // Creates `style` nodes from JS strings
-            "style-loader",
-            // Translates CSS into CommonJS
-            "css-loader",
-            // Compiles Sass to CSS
-            "sass-loader",
+      rules:
+          [
+            ...config.module.rules,
+            {
+              test : /\.s[ac]ss$/i,
+              use : [
+                // Creates `style` nodes from JS strings
+                "style-loader",
+                // Translates CSS into CommonJS
+                "css-loader",
+                // Compiles Sass to CSS
+                "sass-loader",
+              ],
+            },
           ],
-        },
-      ],
     },
   }
 }
