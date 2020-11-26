9d1f96ac9d9ccea557d79a184ba9e1c43571b162
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-11-26 12:33:31 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+echo "Hello from Cezerin3..."
+echo "Checking for nodejs and yarnpkg"
+node -v
+yarn -v
+echo "................................"
+
+echo "Installing Packages"
+yarn
+echo "................................"
+
+echo "Adding NX Cli"
+yarn global add nx
+echo "................................"
+
+echo "Building Cezerin3"
+nx run admin:build
+nx run store:build
+echo "................................"
+
+echo "Starting store..."
+echo "You can see the the store at http://localhost:3000"
+echo "Thankyou for using Cezerin3"
+echo "And please consider monetery support if you gained value from this"
+echo "because it takes years of precious human time to make this..."
+echo "If you have any quetions, post them at issues section or,"
+echo "cezerin3@yahoo.com"
+nx run server:serve --port=3001
+nx run store-ssr:serve --port=3000
