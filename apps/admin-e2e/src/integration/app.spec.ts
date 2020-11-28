5de5d6129401209469f17476ca153280b31a8f07
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-11-28 12:20:48 +0530

    writing tests

--- apps/admin-e2e/src/integration/app.spec.ts
+++ apps/admin-e2e/src/integration/app.spec.ts
@@ -1,13 +1,32 @@
-import { getGreeting } from "../support/app.po"
+// import { getHeader } from "../support/app.po"
 
 describe("admin", () => {
-  beforeEach(() => cy.visit("/"))
+  beforeEach(() => cy.visit("/admin"))
 
-  it("should display welcome message", () => {
+  it("should display header", () => {
     // Custom command example, see `../support/commands.ts` file
-    cy.login("my-email@something.com", "myPassword")
+    // cy.login("my-email@something.com", "myPassword")
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains("Welcome to admin!")
+    // getHeader().contains("Dashboard")
+
+    cy.contains("Dashboard")
+    cy.contains("Orders")
+    cy.contains("Sales")
+
+    cy.visit("/admin/products")
+    cy.contains("Products")
+    cy.contains("Clothing")
+    cy.contains("Product A")
+
+    cy.visit("/admin/orders")
+    cy.contains("Orders")
+    cy.contains("1000")
+    cy.contains("All Statuses")
+
+    cy.visit("/admin/customers")
+    cy.contains("Customers")
+    cy.contains("Himadu")
+    cy.contains("All Customers")
   })
 })
