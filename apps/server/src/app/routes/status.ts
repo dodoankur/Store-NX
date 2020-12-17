23a36e673912c7087ad91f5b3929f043f747e652
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-12-17 19:07:08 +0530

    Update status.ts

--- apps/server/src/app/routes/status.ts
+++ apps/server/src/app/routes/status.ts
@@ -1,4 +1,6 @@
+import { exec } from "child_process"
 import { NextFunction, Request, Response, Router } from "express"
+import winston from "winston"
 import security from "../lib/security"
 
 const router = Router()
@@ -12,7 +14,7 @@ router
   .get(
     "/v1/status/update",
     security.checkUserScope.bind(this, security.scope.READ_SETTINGS),
-    getStatus.bind(this)
+    getUpdates.bind(this)
   )
 
 function getStatus(req: Request, res: Response, next: NextFunction) {
@@ -20,10 +22,39 @@ function getStatus(req: Request, res: Response, next: NextFunction) {
 }
 
 function getUpdates(req: Request, res: Response, next: NextFunction) {
-  // exec("git pull")
-  // exec("nx build:server")
-  // exec("nx build:admin")
-  // exec("nx build:store")
+  exec("git pull", (err, stdout, stderr) => {
+    if (err) {
+      winston.error(err)
+      return
+    }
+    winston.info(stdout)
+  })
+
+  exec("nx build server", (err, stdout, stderr) => {
+    if (err) {
+      winston.error(err)
+      return
+    }
+    winston.info(stdout)
+  })
+
+  exec("nx build admin", (err, stdout, stderr) => {
+    if (err) {
+      winston.error(err)
+      return
+    }
+    winston.info(stdout)
+  })
+
+  exec("nx build store", (err, stdout, stderr) => {
+    if (err) {
+      winston.error(err)
+      return
+    }
+    winston.info(stdout)
+  })
+
+  winston.info("Cezerin3 has been updated!")
   res.status(200).send("Cezerin3 has been updated!")
 }
 
