7203c0554378564d32699c2b46fdcd909c00afba
Restyled.io <commits@restyled.io>
2020-12-31 06:13:32 +0000

    Restyled by clang-format

--- webpack.config.js
+++ webpack.config.js
@@ -2,83 +2,83 @@ const path = require("path")
 const webpack = require("webpack")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
-const { CleanWebpackPlugin } = require("clean-webpack-plugin")
-const { GenerateSW } = require("workbox-webpack-plugin")
+const {CleanWebpackPlugin} = require("clean-webpack-plugin")
+const {GenerateSW} = require("workbox-webpack-plugin")
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin")
 
 module.exports = {
-  entry: {
-    app: "./src/store/client/index.tsx",
+  entry : {
+    app : "./src/store/client/index.tsx",
   },
 
-  performance: {
-    hints: false,
+  performance : {
+    hints : false,
   },
 
-  output: {
-    publicPath: "/",
-    path: path.resolve(__dirname, "theme"),
-    filename: "assets/js/[name]-[chunkhash].js",
-    chunkFilename: "assets/js/[name]-[chunkhash].js",
+  output : {
+    publicPath : "/",
+    path : path.resolve(__dirname, "theme"),
+    filename : "assets/js/[name]-[chunkhash].js",
+    chunkFilename : "assets/js/[name]-[chunkhash].js",
   },
 
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          chunks: "initial",
-          name: "theme",
-          test: "theme",
-          enforce: true,
+  optimization : {
+    splitChunks : {
+      cacheGroups : {
+        vendor : {
+          chunks : "initial",
+          name : "theme",
+          test : "theme",
+          enforce : true,
         },
       },
     },
   },
 
-  resolve: {
-    alias: {
-      "@store/client": path.resolve("libs/client/src/index.ts"),
-      "@store/config": path.resolve("libs/config/src/index.ts"),
-      "@store/theme": path.resolve("libs/theme/src/index.ts"),
+  resolve : {
+    alias : {
+      "@store/client" : path.resolve("libs/client/src/index.ts"),
+      "@store/config" : path.resolve("libs/config/src/index.ts"),
+      "@store/theme" : path.resolve("libs/theme/src/index.ts"),
     },
-    extensions: [".tsx", ".ts", ".js"],
+    extensions : [ ".tsx", ".ts", ".js" ],
   },
 
-  module: {
-    rules: [
+  module : {
+    rules : [
       {
-        test: /\.tsx?$/,
-        use: { loader: "ts-loader", options: { transpileOnly: true } },
-        exclude: /node_modules/,
+        test : /\.tsx?$/,
+        use : {loader : "ts-loader", options : {transpileOnly : true}},
+        exclude : /node_modules/,
       },
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/env", "@babel/react"],
-            plugins: ["transform-class-properties"],
+        test : /\.(js|jsx)$/,
+        exclude : /node_modules/,
+        use : {
+          loader : "babel-loader",
+          options : {
+            presets : [ "@babel/env", "@babel/react" ],
+            plugins : [ "transform-class-properties" ],
           },
         },
       },
       {
-        test: /\.css$/,
-        use: [
+        test : /\.css$/,
+        use : [
           MiniCssExtractPlugin.loader,
           {
-            loader: "css-loader",
-            options: {
-              modules: false,
-              importLoaders: true,
+            loader : "css-loader",
+            options : {
+              modules : false,
+              importLoaders : true,
             },
           },
           "postcss-loader",
         ],
       },
       {
-        test: /\.s[ac]ss$/,
-        use: [
+        test : /\.s[ac]ss$/,
+        use : [
           MiniCssExtractPlugin.loader,
           "css-loader",
           "postcss-loader",
@@ -86,34 +86,34 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
+        test : /\.(png|jpe?g|gif)$/i,
+        use : [
           {
-            loader: "file-loader",
+            loader : "file-loader",
           },
         ],
       },
       {
-        test: /\.(png|jpg|gif|otf|eot|png|svg|ttf|woff|woff2)$/i,
-        use: [
+        test : /\.(png|jpg|gif|otf|eot|png|svg|ttf|woff|woff2)$/i,
+        use : [
           {
-            loader: "url-loader",
-            options: {
-              limit: 8192,
+            loader : "url-loader",
+            options : {
+              limit : 8192,
             },
           },
         ],
       },
       {
-        test: /\.svg$/,
-        loader: "svg-inline-loader",
+        test : /\.svg$/,
+        loader : "svg-inline-loader",
       },
     ],
   },
 
-  plugins: [
+  plugins : [
     new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: [
+      cleanOnceBeforeBuildPatterns : [
         path.resolve("theme/assets/js/*-*.js"),
         // path.resolve("theme/dist/*"),
         path.resolve("theme/assets/sw.js"),
@@ -122,51 +122,51 @@ module.exports = {
     }),
     // new ForkTsCheckerWebpackPlugin({ async: true }),
     new MiniCssExtractPlugin({
-      filename: "assets/css/bundle-[contenthash].css",
-      chunkFilename: "assets/css/bundle-[contenthash].css",
+      filename : "assets/css/bundle-[contenthash].css",
+      chunkFilename : "assets/css/bundle-[contenthash].css",
     }),
     new HtmlWebpackPlugin({
-      template: "theme/index.html",
-      inject: "body",
-      filename: "assets/index.html",
+      template : "theme/index.html",
+      inject : "body",
+      filename : "assets/index.html",
     }),
     new GenerateSW({
-      swDest: "assets/sw.js",
-      clientsClaim: true,
-      skipWaiting: true,
-      exclude: [/\.html$/, /\.svg$/],
-      runtimeCaching: [
+      swDest : "assets/sw.js",
+      clientsClaim : true,
+      skipWaiting : true,
+      exclude : [ /\.html$/, /\.svg$/],
+      runtimeCaching : [
         {
-          urlPattern: new RegExp("/(images|assets|admin-assets)/"),
-          handler: "NetworkFirst",
+          urlPattern : new RegExp("/(images|assets|admin-assets)/"),
+          handler : "NetworkFirst",
         },
         {
-          urlPattern: new RegExp("/api/"),
-          handler: "NetworkOnly",
+          urlPattern : new RegExp("/api/"),
+          handler : "NetworkOnly",
         },
         {
-          urlPattern: new RegExp("/ajax/payment_form_settings"),
-          handler: "NetworkOnly",
+          urlPattern : new RegExp("/ajax/payment_form_settings"),
+          handler : "NetworkOnly",
         },
         {
-          urlPattern: new RegExp("/"),
-          handler: "NetworkFirst",
-          options: {
-            networkTimeoutSeconds: 10,
+          urlPattern : new RegExp("/"),
+          handler : "NetworkFirst",
+          options : {
+            networkTimeoutSeconds : 10,
           },
         },
       ],
     }),
     new webpack.BannerPlugin({
-      banner: `Created: ${new Date().toUTCString()}`,
-      raw: false,
-      entryOnly: false,
+      banner : `Created: ${new Date().toUTCString()}`,
+      raw : false,
+      entryOnly : false,
     }),
   ],
 
-  stats: {
-    children: false,
-    entrypoints: false,
-    modules: false,
+  stats : {
+    children : false,
+    entrypoints : false,
+    modules : false,
   },
 }
--- webpack.config.server.js
+++ webpack.config.server.js
@@ -1,85 +1,85 @@
 const path = require("path")
 const webpack = require("webpack")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
-const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const {CleanWebpackPlugin} = require("clean-webpack-plugin")
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin")
 const nodeExternals = require("webpack-node-externals")
 
 module.exports = {
-  target: "node",
-  entry: {
-    server: "./src/store/server/index.ts",
+  target : "node",
+  entry : {
+    server : "./src/store/server/index.ts",
   },
 
-  performance: {
-    hints: false,
+  performance : {
+    hints : false,
   },
 
-  output: {
-    publicPath: "/",
-    path: path.resolve(__dirname, "dist/store/server"),
+  output : {
+    publicPath : "/",
+    path : path.resolve(__dirname, "dist/store/server"),
     // filename: "[name]-[chunkhash].js",
     // chunkFilename: "[name]-[chunkhash].js",
-    filename: "index.js",
+    filename : "index.js",
   },
-  externals: [nodeExternals()],
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          chunks: "initial",
-          name: "theme",
-          test: "theme",
-          enforce: true,
+  externals : [ nodeExternals() ],
+  optimization : {
+    splitChunks : {
+      cacheGroups : {
+        vendor : {
+          chunks : "initial",
+          name : "theme",
+          test : "theme",
+          enforce : true,
         },
       },
     },
   },
 
-  resolve: {
-    alias: {
-      "@store/client": path.resolve("libs/client/src/index.ts"),
-      "@store/config": path.resolve("libs/config/src/index.ts"),
-      "@store/theme": path.resolve("libs/theme/src/index.ts"),
+  resolve : {
+    alias : {
+      "@store/client" : path.resolve("libs/client/src/index.ts"),
+      "@store/config" : path.resolve("libs/config/src/index.ts"),
+      "@store/theme" : path.resolve("libs/theme/src/index.ts"),
     },
-    extensions: [".tsx", ".ts", ".js"],
+    extensions : [ ".tsx", ".ts", ".js" ],
   },
 
-  module: {
-    rules: [
+  module : {
+    rules : [
       {
-        test: /\.tsx?$/,
-        use: { loader: "ts-loader", options: { transpileOnly: true } },
-        exclude: /node_modules/,
+        test : /\.tsx?$/,
+        use : {loader : "ts-loader", options : {transpileOnly : true}},
+        exclude : /node_modules/,
       },
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/env", "@babel/react"],
-            plugins: ["transform-class-properties"],
+        test : /\.(js|jsx)$/,
+        exclude : /node_modules/,
+        use : {
+          loader : "babel-loader",
+          options : {
+            presets : [ "@babel/env", "@babel/react" ],
+            plugins : [ "transform-class-properties" ],
           },
         },
       },
       {
-        test: /\.css$/,
-        use: [
+        test : /\.css$/,
+        use : [
           MiniCssExtractPlugin.loader,
           {
-            loader: "css-loader",
-            options: {
-              modules: false,
-              importLoaders: true,
+            loader : "css-loader",
+            options : {
+              modules : false,
+              importLoaders : true,
             },
           },
           "postcss-loader",
         ],
       },
       {
-        test: /\.s[ac]ss$/,
-        use: [
+        test : /\.s[ac]ss$/,
+        use : [
           MiniCssExtractPlugin.loader,
           "css-loader",
           "postcss-loader",
@@ -87,32 +87,32 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
+        test : /\.(png|jpe?g|gif)$/i,
+        use : [
           {
-            loader: "file-loader",
+            loader : "file-loader",
           },
         ],
       },
       {
-        test: /\.(png|jpg|gif|otf|eot|png|svg|ttf|woff|woff2)$/i,
-        use: [
+        test : /\.(png|jpg|gif|otf|eot|png|svg|ttf|woff|woff2)$/i,
+        use : [
           {
-            loader: "url-loader",
-            options: {
-              limit: 8192,
+            loader : "url-loader",
+            options : {
+              limit : 8192,
             },
           },
         ],
       },
       {
-        test: /\.svg$/,
-        loader: "svg-inline-loader",
+        test : /\.svg$/,
+        loader : "svg-inline-loader",
       },
     ],
   },
 
-  plugins: [
+  plugins : [
     // new CleanWebpackPlugin({
     //   cleanOnceBeforeBuildPatterns: [
     //     path.resolve("theme/assets/js/*-*.js"),
@@ -123,19 +123,19 @@ module.exports = {
     // }),
     // new ForkTsCheckerWebpackPlugin({ async: true }),
     new MiniCssExtractPlugin({
-      filename: "assets/css/bundle-[contenthash].css",
-      chunkFilename: "assets/css/bundle-[contenthash].css",
+      filename : "assets/css/bundle-[contenthash].css",
+      chunkFilename : "assets/css/bundle-[contenthash].css",
     }),
     new webpack.BannerPlugin({
-      banner: `Created: ${new Date().toUTCString()}`,
-      raw: false,
-      entryOnly: false,
+      banner : `Created: ${new Date().toUTCString()}`,
+      raw : false,
+      entryOnly : false,
     }),
   ],
 
-  stats: {
-    children: false,
-    entrypoints: false,
-    modules: false,
+  stats : {
+    children : false,
+    entrypoints : false,
+    modules : false,
   },
 }
