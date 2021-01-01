c77664b922bb664e6368d00d03673809831349cb
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2021-01-01 14:46:22 +0530

    Update readIndexHtml.ts

--- src/store/server/readIndexHtml.ts
+++ src/store/server/readIndexHtml.ts
@@ -2,11 +2,11 @@ import fse from "fs-extra"
 import path from "path"
 import winston from "winston"
 
-const FILE_PATH = path.resolve("theme/assets/index.html")
+const filepath = path.resolve("theme/assets/index.html")
 export let indexHtml = null
 
 try {
-  indexHtml = fse.readFileSync(FILE_PATH, "utf8")
+  indexHtml = fse.readFileSync(filepath, "utf8")
 } catch (err) {
-  winston.error("Fail to read file", FILE_PATH, err)
+  winston.error("Fail to read file", filepath, err)
 }
